c88773c5a813d78ae160762133f922b82632241d
Fridah Kanario <fkanario@tracom.co.ke>
2019-03-25 10:18:32 +0300

    first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heros';
+  title = 'Tour of Heroes';
+
 }
+
